59a2296b82851afa603e0d53178952dc24945a53
ollau1 <95914821+ollau1@users.noreply.github.com>
2022-07-27 08:50:11 +0200

    Indsat kommentarer og gjorde consolewritelines pænere

--- sandsynlighedberegner test/Dice.cs
+++ sandsynlighedberegner test/Dice.cs
@@ -9,6 +9,7 @@ namespace sandsynlighedberegner_test
 {
     public class Dice
     {
+        //Metode til valg af hvor mange terninger der skal slås/kastes.
         public static int GetDiceAmount()
         {
             Console.WriteLine("Hvor mange terninger vil du kaste med?");
@@ -17,18 +18,28 @@ namespace sandsynlighedberegner_test
             DiceProbability(dice, MakeDice(dice));
             return dice;
         }
+        //Metode til at lave 1-5 terninger og return summen.
         public static int MakeDice(int dice)
         {
             int diceSum = 0;
+            if (dice == 1)
+            {
+                Random random = new Random();
+                int dice1 = random.Next(1, 7);
+                Console.WriteLine("Dice 1 = " + dice1);
+                diceSum = dice1;
+                Console.WriteLine(diceSum);
+                return diceSum;
+            }
             if (dice == 2)
             {
                 Random random = new Random();
                 int dice1 = random.Next(1, 7);
                 int dice2 = random.Next(1, 7);
-                Console.WriteLine(dice1);
-                Console.WriteLine(dice2);
+                Console.WriteLine("Dice 1 = " + dice1);
+                Console.WriteLine("Dice 2 = " + dice2);
                 diceSum = dice1 + dice2;
-                Console.WriteLine(diceSum);
+                Console.WriteLine("Dice sum = " + diceSum);
                 return diceSum;
             }
             else if (dice == 3)
@@ -37,11 +48,11 @@ namespace sandsynlighedberegner_test
                 int dice1 = random.Next(1, 7);
                 int dice2 = random.Next(1, 7);
                 int dice3 = random.Next(1, 7);
-                Console.WriteLine(dice1);
-                Console.WriteLine(dice2);
-                Console.WriteLine(dice3);
+                Console.WriteLine("Dice 1 = " + dice1);
+                Console.WriteLine("Dice 2 = " + dice2);
+                Console.WriteLine("Dice 3 = " + dice3);
                 diceSum = dice1 + dice2 + dice3;
-                Console.WriteLine(diceSum);
+                Console.WriteLine("Dice sum = " + diceSum);
                 return diceSum;
             }
             else if (dice == 4)
@@ -51,12 +62,12 @@ namespace sandsynlighedberegner_test
                 int dice2 = random.Next(1, 7);
                 int dice3 = random.Next(1, 7);
                 int dice4 = random.Next(1, 7);
-                Console.WriteLine(dice1);
-                Console.WriteLine(dice2);
-                Console.WriteLine(dice3);
-                Console.WriteLine(dice4);
+                Console.WriteLine("Dice 1 = " + dice1);
+                Console.WriteLine("Dice 2 = " + dice2);
+                Console.WriteLine("Dice 3 = " + dice3);
+                Console.WriteLine("Dice 4 = " + dice4);
                 diceSum = dice1 + dice2 + dice3 + dice4;
-                Console.WriteLine(diceSum);
+                Console.WriteLine("Dice sum = " + diceSum);
                 return diceSum;
             }
             else if (dice == 5)
@@ -67,29 +78,40 @@ namespace sandsynlighedberegner_test
                 int dice3 = random.Next(1, 7);
                 int dice4 = random.Next(1, 7);
                 int dice5 = random.Next(1, 7);
-                Console.WriteLine(dice1);
-                Console.WriteLine(dice2);
-                Console.WriteLine(dice3);
-                Console.WriteLine(dice4);
-                Console.WriteLine(dice5);
+                Console.WriteLine("Dice 1 = " + dice1);
+                Console.WriteLine("Dice 2 = " + dice2);
+                Console.WriteLine("Dice 3 = " + dice3);
+                Console.WriteLine("Dice 4 = " + dice4);
+                Console.WriteLine("Dice 5 = " + dice5);
                 diceSum = dice1 + dice2 + dice3 + dice4 + dice5;
-                Console.WriteLine(diceSum);
+                Console.WriteLine("Dice sum = " + diceSum);
                 return diceSum;
             }
-            else return diceSum;
+            else
+            {
+                Console.WriteLine("DER ER IKKE SÅ MANGE TERNINGER");
+                return diceSum;
+            }
+                    
         }
+        //Metode til at udregne og udskrive chancen for slaget.
         static void DiceProbability(int dice, int diceSum)
         {
+
             double[,] probabilities = new double[dice + 1, 6 * dice + 1];
             for (int i = 1; i <= 6; i++)
+                //vi tager 2. plads og di
                 probabilities[1, i] = 1 / 6.0;
+            //Her beregner vi alle mulighederne
             for (int i = 2; i <= dice; i++)
                 for (int j = i - 1; j <= 6 * (i - 1); j++)
                     for (int k = 1; k <= 6; k++)
                     {
+                        //Her indsætter vi dem i vores multidimensional array "probabilities"
                         probabilities[i, j + k] += (probabilities[i - 1, j] *
                                          probabilities[1, k]);
                     }
+            //Her udskriver vi % chancen for det slag man slog
             for (int i = dice; i <= 6 * dice; i++)
             {
                 if (diceSum == i)
--- sandsynlighedberegner test/Program.cs
+++ sandsynlighedberegner test/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using static sandsynlighedberegner_test.Dice;
 
-namespace sandsynlighedberegner_test // Note: actual namespace depends on the project name.
+namespace sandsynlighedberegner_test
 {
     internal class Program : sandsynlighedberegner_test.Dice
     {
         static void Main(string[] args)
         {
+            //Kald af GetDiceAmount metode
             GetDiceAmount();
             Console.ReadKey();
         }
